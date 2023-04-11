1cdf3feaeed365024f51f1c6136b72895608cb4d
Camilo2297M <130512084+Camilo2297M@users.noreply.github.com>
2023-04-11 17:09:02 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function validar() {
+    Nombre = document.getElementById("Nombre").value;
+    Apellido = document.getElementById("Apellido").value;
+    Cedula = document.getElementById("Cedula").value;
+    Telefono = document.getElementById("Telefono").value;
+    
+    if(Nombre.length == 0 ){
+        alert("''''''''''''''''''''");
+    }
+    
+    
+}
